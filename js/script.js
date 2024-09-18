9fffb054bedb46fddf481318e3c7b945a27eaa76
mai-2 <00eris@gmail.com>
2024-09-19 08:43:22 +0900

    0918

--- js/script.js
+++ js/script.js
@@ -1,50 +1,31 @@
-
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
-  var topBtn = $('.pagetop');
-  topBtn.hide();
-
-  // ボタンの表示設定
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
-      topBtn.fadeIn();
-    } else {
-      // 画面が指定pxより上ならボタンを非表示
-      topBtn.fadeOut();
-    }
-  });
-
-  // ボタンをクリックしたらスクロールして上に戻る
-  topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
-    return false;
-  });
-
-  //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-
-  });
-
-
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
-  $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
-  });
-
+jQuery("#js-drawer-icon").on("click", function(e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+  jQuery("#js-body").toggleClass("is-checked");
 });
+
+jQuery('#js-drawer-content a[href^="#"]').on("click", function(e) {
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+  jQuery("#js-body").removeClass("is-checked");
+});
+
+$('a[href^="#"]').on('click', function () {
+  // #から始まるURLがクリックされた時
+  // .headerクラスがついた要素の高さを取得
+  var header = jQuery(".header").innerHeight(); // 移動速度を指定（ミリ秒）
+
+  var speed = 300; // hrefで指定されたidを取得
+
+  var id = $(this).attr("href"); // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
+
+  var target = $("#" == id ? "html" : id); // トップからの距離からヘッダー分の高さを引く
+
+  var position = $(target).offset().top - header; // その分だけ移動すればヘッダーと被りません
+
+  $("html, body").animate({
+    scrollTop: position
+  }, speed);
+  return false;
+}); // トップへ戻るボタン
